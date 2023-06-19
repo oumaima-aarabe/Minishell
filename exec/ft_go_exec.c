202c8bd041c2ffd281509593b153e1a7695f91b1
AYoubZarda <zardaayoub1@gmail.com>
2023-06-20 00:02:53 +0100

    ll

--- exec/ft_go_exec.c
+++ exec/ft_go_exec.c
@@ -6,7 +6,7 @@
 /*   By: azarda <azarda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 15:11:26 by azarda            #+#    #+#             */
-/*   Updated: 2023/06/19 20:38:53 by azarda           ###   ########.fr       */
+/*   Updated: 2023/06/19 20:54:35 by azarda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,18 @@
 
 int ft_execut_cmd(splitnode *cmd, t_env *env)
 {
-	// int i = 0;
+	int i = 0;
 
-	// while(cmd && cmd->next)
-	// {
-	// 	while(cmd->splitdata[i])
-	// 	printf("cmd -->%s\n", cmd->splitdata[i++]);
-	// 	// printf("in --> %d\n", cmd->in);
-	// 	// printf("out --> %d\n", cmd->out);
-	// 	cmd = cmd->next;
-	// 	i = 0;
-	// }
+	while(cmd && cmd->next)
+	{
+		
+		while(cmd->splitdata[i])
+			printf("cmd -->%s\n", cmd->splitdata[i++]);
+		// printf("in --> %d\n", cmd->in);
+		// printf("out --> %d\n", cmd->out);
+		cmd = cmd->next;
+		i = 0;
+	}
 	// (ft_lstsize((t_env *)cmd) == 1)
 	// {
 		if(ft_execut_bultins(cmd, env))
